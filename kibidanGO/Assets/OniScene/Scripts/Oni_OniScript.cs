b540a106a011bd638a56b335db4b8e3a2915d7c1
SaeMatayoshi <c18016@std.it-college.ac.jp>
2019-06-27 12:24:03 +0900

    Oni_OniScript、Oni_PlayerScriptを修正 Oni_GameControllerScriptを追加

--- kibidanGO/Assets/OniScene/Scripts/Oni_OniScript.cs
+++ kibidanGO/Assets/OniScene/Scripts/Oni_OniScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Oni_OniScript : MonoBehaviour
 {
+    Oni_PlayerScript m_playerScript;
+
     private int m_oniHP = 500;
     public int OniHP
     {
@@ -13,36 +15,46 @@ public class Oni_OniScript : MonoBehaviour
 
     private int m_attackRandom = 0;
 
-    private bool m_oniupper;
-    private bool m_onimiddle;
-    private bool m_onilower;
+    [System.NonSerialized] public bool m_oniupper = false;
+    [System.NonSerialized] public bool m_onimiddle = false;
+    [System.NonSerialized] public bool m_onilower = false;
     
     // Start is called before the first frame update
     void Start()
     {
+        // m_playerScript = GameObject.FindGameObjectWithTag("")
         StartCoroutine("OniAttackRandom");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(m_attackRandom);
+        Debug.Log("上： " + m_oniupper + "中： " + m_onimiddle + "下： " + m_onilower);
     }
 
-    IEnumerator OniAttackRandom()
+    public IEnumerator OniAttackRandom()
     {
+        Debug.Log("OniCor Start");
         while (true)
         {
-            yield return new WaitForSeconds(1.0f);
             m_attackRandom = Random.Range(1, 4);
+            Debug.Log("Rand " + m_attackRandom);
 
             if (m_attackRandom == 1)
+            {
                 m_oniupper = true;
+                yield return new WaitForSeconds(3.0f);
+            }
             else if (m_attackRandom == 2)
+            {
                 m_onimiddle = true;
+                yield return new WaitForSeconds(3.0f);
+            }
             else if (m_attackRandom == 3)
+            {
                 m_onilower = true;
+                yield return new WaitForSeconds(3.0f);
+            }
         }
     }
-
 }
--- kibidanGO/Assets/OniScene/Scripts/Oni_PlayerScript.cs
+++ kibidanGO/Assets/OniScene/Scripts/Oni_PlayerScript.cs
@@ -6,6 +6,12 @@ public class Oni_PlayerScript : MonoBehaviour
 {
     Oni_OniScript m_oniScript;
 
+    [System.NonSerialized] public bool m_onDogButton = false;
+    [System.NonSerialized] public bool m_onMonkeyButton = false;
+    [System.NonSerialized] public bool m_onPheasantButton = false;
+
+    int m_playerAttack;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,20 +21,58 @@ public class Oni_PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("イヌ： " + m_onDogButton + "サル： " + m_onMonkeyButton + "キジ： " + m_onPheasantButton);
     }
-    public void OnClickDogButton()
+
+    public void OnClickedDogButton()
     {
+        m_onDogButton = true;
+        m_onMonkeyButton = false;
+        m_onPheasantButton = false;
+        // StartCoroutine("OnAnyButtonClick");
         Debug.Log("イヌ");
     }
 
-    public void OnClickMonkeyButton()
+    public void OnClickedMonkeyButton()
     {
+        m_onDogButton = false;
+        m_onMonkeyButton = true;
+        m_onPheasantButton = false;
         Debug.Log("サル");
     }
 
-    public void OnClickPheasantButton()
+    public void OnClickedPheasantButton()
     {
+        m_onDogButton = false;
+        m_onMonkeyButton = false;
+        m_onPheasantButton = true;
         Debug.Log("キジ");
     }
+
+    void PlayerAttack()
+    {
+        if(m_onDogButton && !m_oniScript.m_onilower)
+        {
+            m_playerAttack = 5;
+            m_oniScript.OniHP -= m_playerAttack;
+            Debug.Log("イヌ　" + m_playerAttack + "ダメージ");
+        }
+    }
+
+    /* IEnumerator OnAnyButtonClick()
+    {
+        Debug.Log("PlaCor Start");
+        if (m_onDogButton && !m_onMonkeyButton && !m_onPheasantButton)
+        {
+            yield return m_oniScript.StartCoroutine("OniAttackRandom");
+        }
+        else if (m_onMonkeyButton)
+        {
+            yield return new WaitForSeconds(2.0f);
+        }
+        else if (m_onPheasantButton)
+        {
+            yield return new WaitForSeconds(2.0f);
+        }
+    } */
 }
