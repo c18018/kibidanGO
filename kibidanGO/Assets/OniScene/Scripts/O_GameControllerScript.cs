861d3e375be0043e48454a020cd2e7a3ea5211b1
SaeMatayoshi <c18016@std.it-college.ac.jp>
2019-07-09 11:14:40 +0900

    鬼のHPが減ると、ボタンを選択する時間が短くなるようにした。

--- kibidanGO/Assets/OniScene/Scripts/O_GameControllerScript.cs
+++ kibidanGO/Assets/OniScene/Scripts/O_GameControllerScript.cs
@@ -8,32 +8,70 @@ public class O_GameControllerScript : MonoBehaviour
     float time = 0;
     O_OniScript oniSc;
     O_PlayerScript plaSc;
+    
+    [System.NonSerialized] public float waitTime = 0;
 
-    string oniHp = "500";
+    string oniHp = "200";
+
+    Text attackText;
 
     // Start is called before the first frame update
     void Start()
     {
         oniSc = GameObject.FindGameObjectWithTag("Oni").GetComponent<O_OniScript>();
         plaSc = GameObject.FindGameObjectWithTag("GameController").GetComponent<O_PlayerScript>();
+        // あとでTagをかえる↓
+        attackText = GameObject.FindGameObjectWithTag("Player").GetComponent<Text>();
+        attackText.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        Timer();
+        ChangeText();
     }
 
-    void Timer()
+
+
+    public void Timer()
     {
-        time += 1 * Time.deltaTime;
-        //Debug.Log((int)time);
+        waitTime -= 1 * Time.deltaTime;
     }
 
+    void ChangeText()
+    {
+        if (oniSc.m_oniupper)
+        {
+            attackText.text = " うえ　　からこうげきがくる!!";
+            if (plaSc.countTime)
+            {
+                attackText.text = " うえ　　からこうげきがくる!!　" + (int)waitTime;
+            }
+        }
+        else if (oniSc.m_onimiddle)
+        {
+            attackText.text = "まんなか　からこうげきがくる!!";
+            if (plaSc.countTime)
+            {
+                //Timer();
+                attackText.text = "まんなか　からこうげきがくる!!　" + (int)waitTime;
+            }
+        }
+        else if (oniSc.m_onilower)
+        {
+            attackText.text = "　した　　からこうげきがくる!!";
+            if (plaSc.countTime)
+            {
+                //Timer();
+                attackText.text = "　した　　からこうげきがくる!!　" + (int)waitTime;
+            }
+        }
+    }
+   
     private void OnGUI()
     {
         GUI.skin.label.fontSize = 50;
         GUI.Label(new Rect(1700, 100, 500, 300), System.Convert.ToString(oniSc.OniHP) + " / " + oniHp);
-        GUI.Label(new Rect(100, 100, 500, 300), System.Convert.ToString((int)time));
+        GUI.Label(new Rect(50, 100, 500, 300), System.Convert.ToString((int)time));
     }
 }
--- kibidanGO/Assets/OniScene/Scripts/O_OniScript.cs
+++ kibidanGO/Assets/OniScene/Scripts/O_OniScript.cs
@@ -7,7 +7,7 @@ public class O_OniScript : MonoBehaviour
 {
     O_PlayerScript m_playerScript;
 
-    private int m_oniHP = 500;
+    private int m_oniHP = 200;
     public int OniHP
     {
         get { return this.m_oniHP; }
@@ -15,8 +15,7 @@ public class O_OniScript : MonoBehaviour
     }
 
     private int m_attackRandom = 0;
-    Text m_attackText;
-
+    
     [System.NonSerialized] public bool m_oniupper = false;
     [System.NonSerialized] public bool m_onimiddle = false;
     [System.NonSerialized] public bool m_onilower = false;
@@ -25,16 +24,14 @@ public class O_OniScript : MonoBehaviour
     void Start()
     {
         m_playerScript = GameObject.FindGameObjectWithTag("GameController").GetComponent<O_PlayerScript>();
-        m_attackText = GameObject.FindGameObjectWithTag("Player").GetComponent<Text>();
-        m_attackText.text = "";
-
+       
         StartCoroutine("TurnController");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log("上： " + m_oniupper + "中： " + m_onimiddle + "下： " + m_onilower);
+
     }
 
     public IEnumerator TurnController()
@@ -47,58 +44,28 @@ public class O_OniScript : MonoBehaviour
             if(m_attackRandom == 1)
             {
                 m_oniupper = true;
-                m_attackText.text = "　うえ　　からこうげきがくる!!";
             }
             else if(m_attackRandom == 2)
             {
                 m_onimiddle = true;
-                m_attackText.text = "まんなか　からこうげきがくる!!";
             }
             else if(m_attackRandom == 3)
             {
                 m_onilower = true;
-                m_attackText.text = "　した　　からこうげきがくる!!";
             }
-            Debug.Log("Rand : " + m_attackRandom);
+
+            yield return new WaitForSeconds(1.0f);
+
             yield return m_playerScript.StartCoroutine("PlayerButtonSelect");
 
-            m_attackText.text = "";
-
             yield return new WaitForSeconds(2.0f);
+            
             m_playerScript.PlayerAttack();
 
 
             m_oniupper = false;
             m_onimiddle = false;
             m_onilower = false;
-
-            /*if (m_attackRandom == 1)
-            {
-                m_oniupper = true;
-
-                yield return new WaitForSeconds(3.0f);
-
-                m_playerScript.PlayerAttack();
-                m_oniupper = false;
-            }
-            else if (m_attackRandom == 2)
-            {
-                m_onimiddle = true;
-
-                yield return new WaitForSeconds(3.0f);
-
-                m_playerScript.PlayerAttack();
-                m_onimiddle = false;
-            }
-            else if (m_attackRandom == 3)
-            {
-                m_onilower = true;
-
-                yield return new WaitForSeconds(3.0f);
-
-                m_playerScript.PlayerAttack();
-                m_onilower = false;
-            }*/
 
             m_playerScript.dogButton.interactable = false;
             m_playerScript.monkeyButton.interactable = false;
