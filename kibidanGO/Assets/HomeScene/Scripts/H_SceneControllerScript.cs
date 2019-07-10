f034203305f4fed2deb76045d7ab401cafa05ff6
SaeMatayoshi <c18016@std.it-college.ac.jp>
2019-07-10 12:13:47 +0900

    HomeSceneを作成 Canvasの追加 スクリプトの追加

---
+++ kibidanGO/Assets/HomeScene/Scripts/H_SceneControllerScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class H_SceneControllerScript : MonoBehaviour
+{
+    Button friendsButton;
+    Button dangoButton;
+
+    // テスト用ボタン
+    Button testButton;
+
+    // ホームにいるかどうか
+    bool isHomeScene = false;
+
+    // 動物を持っているかどうか
+    bool haveDog = false;
+    bool haveMonkey = false;
+    bool havePheasant = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+        //friendsButton = GameObject.FindGameObjectWithTag("FriendsButton").GetComponent<Button>();
+        //dangoButton = GameObject.FindGameObjectWithTag("DangoButton").GetComponent<Button>();
+
+        // テスト用
+        testButton = GameObject.FindGameObjectWithTag("Player").GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HomeSceneActive();
+
+        OniSceneActive();
+    }
+
+    // あとでARのシーン遷移ができるか試す
+    /*void ChangeARScene()
+    {
+        SceneManager.LoadScene("ARCamera");
+    }*/
+
+    void TestFlag()
+    {
+
+    }
+
+    /*public void OnClickedFriendsButton()
+    {
+
+    }*/
+
+    public void OnClickedTestButton()
+    {
+        SceneManager.LoadScene("OniScene");
+    }
+
+    void HomeSceneActive()
+    {
+        if (SceneManager.GetActiveScene().name == "HomeScene")
+        {
+            isHomeScene = true;
+        }
+        Debug.Log(isHomeScene);
+    }
+
+    void OniSceneActive()
+    {
+        if(SceneManager.GetActiveScene().name == "OniScene")
+        {
+            haveDog = true;
+            float time = 3;
+            if(time > 0)
+            {
+                time -= 1 * Time.deltaTime;
+            }
+
+            if (time <= 0)
+                SceneManager.LoadScene("HomeScene");
+        }
+    }
+}
