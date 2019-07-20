31f8754f30247775c9e9ad956ae688a6d431df45
c18008 <c18008@std.it-college.ac.jp>
2019-07-20 14:34:47 +0900

    ヘラの追加とボタンの調整

--- kibidanGO/Assets/KibiScene/Scripts/MixController.cs
+++ kibidanGO/Assets/KibiScene/Scripts/MixController.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MixController : MonoBehaviour
 {
 
     private int counter = 0;
-    const int counterMax = 5;
+    const int counterMax = 6;
+
+    public GameObject Hera;
+    private float speed;
+    public bool move = false;
+
+    private float timer = 0;
+    public int interval = 1;
+    public bool button = true;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +26,8 @@ public class MixController : MonoBehaviour
         Invoke("Cancel", 3);
 
         Invoke("True", 4);
+
+        speed = 6.0f;
     }
 
     void False()
@@ -34,17 +45,36 @@ public class MixController : MonoBehaviour
         gameObject.SetActive(true);
     }
 
+    private void Update()
+    {
+        if (move)
+        {
+            Hera.transform.RotateAround(new Vector3(0, 0, 0), Vector3.up, speed);
+            timer += Time.deltaTime;
+            if (timer > interval)
+            {
+                timer = 0;
+                counter++;
+                move = false;
+            }
+        }
+    }
     // Update is called once per frame
     public void OnClick()
     {
         Debug.Log("clicked");
-
-        counter++;
+        move = true;
         if (counter >= counterMax)
         {
             Debug.Log("完成！");
             gameObject.SetActive(false);
             SceneManager.LoadScene("Kibi_Finish");
         }
+        Invoke("false", 0);
+
+        Invoke("cancel", 3);
+
+        Invoke("true", 3);
     }
+
 }
--- kibidanGO/Assets/KibiScene/Scripts/SearchController.cs
+++ kibidanGO/Assets/KibiScene/Scripts/SearchController.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class SearchController : MonoBehaviour
 {
+    h_Master Master;
+
+    public bool water;
+    public bool mochi;
+    public bool sugar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Master = GameObject.FindGameObjectWithTag("Master").GetComponent<h_Master>();
+        water = Master.water;
+        mochi = Master.mochi;
+        sugar = Master.sugar;
     }
 
     // Update is called once per frame
